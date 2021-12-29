c30a0b085df852522385e5bb6093c8607fb12729
Kometheus21 <95313393+Kometheus21@users.noreply.github.com>
2021-12-29 18:51:10 +0200

    added modal

--- screens/AllScreen.js
+++ screens/AllScreen.js
@@ -1,36 +1,107 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Modal, Pressable } from "react-native";
 import RoundButton from "../components/RoundButton";
 
 const HomeScreen = () => {
+  const [modalVisible, setModalVisible] = useState(false);
   const handlerLongPress = () => {
     //on longpress open modal
-    alert('Button Long Pressed');
+    setModalVisible(true);
   };
   const handlerPress = () => {
     //on press play the sound
-    alert('Button Pressed');
+    alert("Button Pressed");
   };
   return (
     <View style={styles.container}>
-      <View style={styles.tempHeader}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.container}>
+          <View style={styles.modal}>
+            {/* here goes modal functionality */}
+            <Pressable style={styles.addToFavBtn}>
+              <Text style={styles.modalText}>add to favourites</Text>
+            </Pressable>
+            <Text style={styles.modalText}>-------------------------</Text>
+            <Pressable
+              style={styles.closeBtn}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.modalText}> close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+      <View>
         {/* In here goes logo, name of app and soon navigation */}
         <Text style={styles.heading}>Choose Your favourite!</Text>
       </View>
       <View style={styles.btnContainer}>
-        <RoundButton color={"grey"} text={"First"} onPress={handlerPress} longPress={handlerLongPress}/>
-        <RoundButton color={"blue"} text={"Scnd"} onPress={handlerPress} longPress={handlerLongPress}/>
-        <RoundButton color={"purple"} text={"Third"} onPress={handlerPress} longPress={handlerLongPress}/>
+        <RoundButton
+          color={"grey"}
+          text={"First"}
+          onPress={handlerPress}
+          longPress={handlerLongPress}
+        />
+        <RoundButton
+          color={"blue"}
+          text={"Scnd"}
+          onPress={handlerPress}
+          longPress={handlerLongPress}
+        />
+        <RoundButton
+          color={"purple"}
+          text={"Third"}
+          onPress={handlerPress}
+          longPress={handlerLongPress}
+        />
       </View>
       <View style={styles.btnContainer}>
-        <RoundButton color={"pink"} text={"First"} onPress={handlerPress} longPress={handlerLongPress}/>
-        <RoundButton color={"purple"} text={"Scnd"} onPress={handlerPress} longPress={handlerLongPress}/>
-        <RoundButton color={"green"} text={"Third"} longPress={handlerLongPress}/>
+        <RoundButton
+          color={"pink"}
+          text={"First"}
+          onPress={handlerPress}
+          longPress={handlerLongPress}
+        />
+        <RoundButton
+          color={"purple"}
+          text={"Scnd"}
+          onPress={handlerPress}
+          longPress={handlerLongPress}
+        />
+        <RoundButton
+          color={"green"}
+          text={"Third"}
+          longPress={handlerLongPress}
+        />
       </View>
       <View style={styles.btnContainer}>
-        <RoundButton color={"lightgreen"} text={"First"} onPress={handlerPress} longPress={handlerLongPress}/>
-        <RoundButton color={"yellow"} text={"Scnd"} onPress={handlerPress} longPress={handlerLongPress}/>
-        <RoundButton color={"lightblue"} text={"Third"} onPress={handlerPress} longPress={handlerLongPress}/>
+        <RoundButton
+          color={"lightgreen"}
+          text={"First"}
+          onPress={handlerPress}
+          longPress={handlerLongPress}
+        />
+        <RoundButton
+          color={"yellow"}
+          text={"Scnd"}
+          onPress={handlerPress}
+          longPress={handlerLongPress}
+        />
+        <RoundButton
+          color={"lightblue"}
+          text={"Third"}
+          onPress={handlerPress}
+          longPress={handlerLongPress}
+        />
       </View>
     </View>
   );
@@ -51,9 +122,20 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     color: "grey",
   },
-  tempHeader: {
-    
+  modal: {
+    height: 100,
+    width: "75%",
+    backgroundColor: "pink",
+    borderRadius: 10,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  modalText: {
+    color: "#fff",
+  },
+  closeBtn: {},
+  addToFavBtn: {},
 });
 
 export default HomeScreen;
