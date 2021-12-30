5e3c8f986dc4d388801d94ab324e93a0191f2f69
ViestursK <95314459+ViestursK@users.noreply.github.com>
2021-12-30 23:03:59 +0200

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,14 @@ import { Text, View, StyleSheet } from "react-native";
 const HomeScreen = () => {
   return (
     <View style={styles.cont}>
-      <Text>This is the screen for home info</Text>
+      <Text>Hello, from G2</Text>
+      <Text>
+        This app is a result of hard work and sweat and brain pain. Hope You
+        enjoy playing with this, because we ain't having fun no more. Jokes
+        aside: play the sounds and just appreciate the interface, with the
+        materials, we have provided this is excellent work of Youtube teachers
+        and long scrolls through StackOverFlow. :)
+      </Text>
     </View>
   );
 };
